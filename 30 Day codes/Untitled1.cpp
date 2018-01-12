64a4c565e15657136667f32b7a7885338668b26d
akkshaychauhan <apschauhan700@gmail.com>
2018-01-12 14:22:42 +0530

    Add files via upload

---
+++ 30 Day codes/Untitled1.cpp
@@ -0,0 +1,16 @@
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+int main() {
+   
+    char s[105]; 
+    
+    gets(s);
+    
+    printf("Hello, World.\n");
+    
+    printf("%s",s);
+    
+    
+    return 0;
+}
